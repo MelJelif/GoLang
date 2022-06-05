01868eef2e4ea3d212e32ae0cb1278e6ebe64d15
MelJelif <meljelif@gmail.com>
2022-06-06 00:38:37 +1000

    resolve

--- circle.go
+++ circle.go
@@ -7,7 +7,6 @@ import (
 
 func main() {
 	var a float64
-	b := math.Pi
 
 	fmt.Print("Введите площадь круга: ")
 	_, err := fmt.Scanln(&a)
@@ -15,6 +14,6 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Диаметр окружности %.2f \n", math.Sqrt(a/b)*2)
-
+	fmt.Printf("Диаметр окружности %.2f \n", math.Sqrt(a/math.Pi)*2)
+	fmt.Printf("Длина окружности : %.2f \n", (math.Sqrt(a/math.Pi)*2)*math.Pi)
 }
