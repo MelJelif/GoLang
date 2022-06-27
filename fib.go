79a84f7a77a99f223c18781fb76387496db17eee
MelJelif <meljelif@gmail.com>
2022-06-27 17:32:17 +1000

    dz 5

--- fib.go
+++ fib.go
@@ -4,24 +4,24 @@ import (
 	"fmt"
 )
 
-var rowfib = map[int]int{0: 0, 1: 1}
+type fibMap = map[int]int
 
-func fibfunc(n int) int {
+func fibfunc(rowfib fibMap, n int) int {
 	_, found := rowfib[n]
 	if found != true {
-		rowfib[n] = fibfunc(n-1) + fibfunc(n-2)
+		rowfib[n] = fibfunc(rowfib, n-1) + fibfunc(rowfib, n-2)
 	}
 	return rowfib[n]
 }
 
 func main() {
-
+	var rowfib = map[int]int{0: 0, 1: 1}
 	var n int
 	fmt.Println("Введите искомый пордковый номер числа: ")
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(fibfunc(n))
+	fmt.Println(fibfunc(rowfib, n))
 	fmt.Println(rowfib)
 }
